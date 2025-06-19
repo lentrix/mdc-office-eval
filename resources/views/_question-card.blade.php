39e2d745b95b27946727665ed7d03dabcfda4a53
lentrix <hawkmanlentrix@gmail.com>
2025-06-19 11:40:56 +0800

    require all items

--- resources/views/_question-card.blade.php
+++ resources/views/_question-card.blade.php
@@ -8,7 +8,7 @@
         <div style="display: grid; grid-template-columns: repeat(5, 1fr); gap: 0.5rem;">
         @foreach($selection as $i => $label)
             <div style="display: flex; flex-direction: column; align-items: center; justify-content: start;" class="border rounded p-2 bg-secondary">
-                <input class="form-check-input" type="radio" name="ratings[{{$index}}]" id="ratings{{$index}}_{{ $i }}" value="{{ $i }}">
+                <input class="form-check-input" type="radio" name="ratings[{{$index}}]" id="ratings{{$index}}_{{ $i }}" value="{{ $i }}" required>
                 <label class="form-check-label text-center" for="ratings{{$index}}_{{ $i }}">{{ $label }}</label>
             </div>
         @endforeach
